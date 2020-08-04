1ba1910a9347abfe0909097604a95578eb775aae
Alvaro Leonel <alvaro.lqueiroz@gmail.com>
2020-08-04 19:13:24 -0300

    Resetando migrações

--- piado/views.py
+++ piado/views.py
@@ -3,10 +3,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from piado.models import Piado
 from django.urls import reverse
 
+
 class PiadoCreate(LoginRequiredMixin, CreateView):
     model = Piado
     fields = ['conteudo']
-    template_name = 'home.html'
 
     def form_valid(self, form):
         form.instance.usuario = self.request.user.perfil
