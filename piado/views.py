906e374149fd4678a93e5cf244d34e064d7158fa
Alvaro Leonel <alvaro.lqueiroz@gmail.com>
2020-09-01 22:28:37 -0300

    Adiciona remoção de piados

--- piado/views.py
+++ piado/views.py
@@ -1,8 +1,9 @@
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from piado.models import Piado
-from django.urls import reverse
-
+from django.urls import reverse, reverse_lazy
+from django.http import HttpResponseRedirect
+from django.core.exceptions import PermissionDenied
 
 class PiadoCreate(LoginRequiredMixin, CreateView):
     model = Piado
@@ -14,3 +15,18 @@ class PiadoCreate(LoginRequiredMixin, CreateView):
 
     def get_success_url(self):
         return reverse('perfil', args=[self.request.user.username])
+
+class PiadoDelete(LoginRequiredMixin, DeleteView):
+    model = Piado
+    success_url = reverse_lazy('home')
+
+    def get(self, request, *args, **kwargs):
+        return self.post(self, request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if self.object.usuario.usuario.id != request.request.user.id:
+            raise PermissionDenied
+        success_url = self.get_success_url()
+        self.object.delete()
+        return HttpResponseRedirect(success_url)
