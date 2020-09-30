87fe6a1a4f523ae92dbbd3453a687c9262feda84
Alvaro Leonel <alvaro.lqueiroz@gmail.com>
2020-09-29 22:20:01 -0300

    Adiciona model de hashtag

--- piado/models.py
+++ piado/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Hashtag(models.Model):
+    conteudo = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.conteudo
+
+    @property
+    def count(self):
+        return self.piados.count()
+
+
 class Piado(models.Model):
     proprietario = models.ForeignKey(User, on_delete=models.CASCADE, related_name='piados')
     curtidas = models.ManyToManyField(User, related_name='curtidas', blank=True)
@@ -18,6 +29,7 @@ class Piado(models.Model):
         related_name='comentarios',
         blank=True,
         null=True)
+    hashtags = models.ManyToManyField(Hashtag, related_name='piados')
     conteudo = models.CharField(max_length=400)
 
     data_criacao = models.DateTimeField(auto_now=True, blank=True)
